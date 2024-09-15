064b587d205479e2041f9171374c0bb892b54224
MinseoKangQ <kms02171@naver.com>
2024-09-16 00:53:42 +0900

    :sparkles: 7-4: Users Microservices - loadUserByUsername()

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,16 +1,20 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -21,6 +25,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class WebSecurity {
 
     public static final String MY_IP_ADDRESS = "192.168.0.13";
+    private final Environment env;
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     // Authorization
     @Bean
@@ -50,5 +57,10 @@ public class WebSecurity {
         HttpServletRequest request = context.getRequest();
         String clientIp = request.getRemoteAddr();
         return new AuthorizationDecision(MY_IP_ADDRESS.equals(clientIp));
+    }
+
+    // select pwd from users where email = ?
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }

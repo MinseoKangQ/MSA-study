9a28ed2bc73890ce33ad8256e0f05ae7325fc04a
MinseoKangQ <kms02171@naver.com>
2024-09-22 23:37:47 +0900

    :sparkles: 8-4: Spring Cloud Config - 프로젝트 테스트

--- config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -2,8 +2,13 @@ package com.example.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
+/**
+ * http://localhost:8888/ecommerce/default
+ */
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
